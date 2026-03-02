3b81baa1a7b1e584e76cfce1b39796d0bc4b19bc
mip3x <plombiiiri@yandex.ru>
2026-03-02 22:33:31 +0300

    feat: workqueue added

--- kernel-module/ksysfs-logger.c
+++ kernel-module/ksysfs-logger.c
@@ -1,24 +1,72 @@
+#include <linux/err.h>
+#include <linux/errname.h>
+#include <linux/fs.h>
 #include <linux/jiffies.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/string.h>
 #include <linux/timer.h>
 #include <linux/types.h>
+#include <linux/workqueue.h>
 
 #define TIMEOUT_MS 5000
+
+#define BASEPATH "/var/tmp/test_module"
+static char filename[255] = "log";
 
 static struct timer_list write_msg_timer;
 static atomic_t counter = ATOMIC_INIT(0);
 
-static void write_msg_timer_callback(struct timer_list *unused) {
-    int counter_value = atomic_inc_return(&counter);
-    pr_info("Hello from kernel module (%d)\n", counter_value);
+static struct work_struct write_work;
 
+static void write_work_func(struct work_struct *work) {
+    char fullpath[sizeof(BASEPATH) + sizeof(filename)];
+    struct file *file;
+    loff_t pos = 0;
+    ssize_t bytes_written;
+    int counter_value;
+    char databuf[128];
+    int len;
+
+    // concatenate full path
+    snprintf(fullpath, sizeof(fullpath), "%s/%s", BASEPATH, filename);
+
+    // open file
+    file = filp_open(fullpath, O_RDWR | O_CREAT | O_APPEND, 0644);
+    if (IS_ERR(file)) {
+        long error = PTR_ERR(file);
+        pr_err("filp_open(%s) failed: %ld (%s)\n", fullpath, error, errname(error));
+        return;
+    }
+
+    // get counter value
+    counter_value = atomic_inc_return(&counter);
+
+    // concatenate log message
+    len = snprintf(databuf, sizeof(databuf), "Hello from kernel module (%d)\n", counter_value);
+    pr_info("%s", databuf);
+
+    // write file
+    bytes_written = kernel_write(file, databuf, len, &pos);
+    if (bytes_written < 0) {
+        pr_err("kernel_write failed: %ld\n", bytes_written);
+        filp_close(file, NULL);
+        return;
+    }
+
+    // close file
+    filp_close(file, NULL);
+}
+
+static void write_msg_timer_callback(struct timer_list *unused) {
+    schedule_work(&write_work);
     mod_timer(&write_msg_timer, jiffies + msecs_to_jiffies(TIMEOUT_MS));
 }
 
 static int __init ksysfs_logger_init(void) {
     pr_info("ksysfs logger init\n");
 
+    INIT_WORK(&write_work, write_work_func);
     timer_setup(&write_msg_timer, write_msg_timer_callback, 0);
     mod_timer(&write_msg_timer, jiffies + msecs_to_jiffies(TIMEOUT_MS));
 
@@ -29,6 +77,7 @@ static void __exit ksysfs_logger_exit(void) {
     pr_info("ksysfs logger exit\n");
 
     del_timer_sync(&write_msg_timer);
+    cancel_work_sync(&write_work);
 }
 
 module_init(ksysfs_logger_init);
