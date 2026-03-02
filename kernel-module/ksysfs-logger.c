e81bef953824e457e66b23b0589d4a0d2b951b75
mip3x <plombiiiri@yandex.ru>
2026-03-02 20:42:24 +0300

    chore: renaming

--- kernel-module/ksysfs-logger.c
+++ kernel-module/ksysfs-logger.c
@@ -1,17 +1,17 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 
-static int __init sysfs_logger_init(void) {
+static int __init ksysfs_logger_init(void) {
     pr_info("ksysfs logger init\n");
     return 0;
 }
 
-static void __exit sysfs_logger_exit(void) {
+static void __exit ksysfs_logger_exit(void) {
     pr_info("ksysfs logger exit\n");
 }
 
-module_init(sysfs_logger_init);
-module_exit(sysfs_logger_exit);
+module_init(ksysfs_logger_init);
+module_exit(ksysfs_logger_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("mip3x");
