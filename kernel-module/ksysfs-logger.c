f75feca7acd1d9c19a1f8a2d0a0a83231b897d0a
mip3x <plombiiiri@yandex.ru>
2026-03-02 21:16:13 +0300

    feat: timer added

--- kernel-module/ksysfs-logger.c
+++ kernel-module/ksysfs-logger.c
@@ -1,13 +1,34 @@
+#include <linux/jiffies.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/timer.h>
+#include <linux/types.h>
+
+#define TIMEOUT_MS 5000
+
+static struct timer_list write_msg_timer;
+static atomic_t counter = ATOMIC_INIT(0);
+
+static void write_msg_timer_callback(struct timer_list *unused) {
+    int counter_value = atomic_inc_return(&counter);
+    pr_info("Hello from kernel module (%d)\n", counter_value);
+
+    mod_timer(&write_msg_timer, jiffies + msecs_to_jiffies(TIMEOUT_MS));
+}
 
 static int __init ksysfs_logger_init(void) {
     pr_info("ksysfs logger init\n");
+
+    timer_setup(&write_msg_timer, write_msg_timer_callback, 0);
+    mod_timer(&write_msg_timer, jiffies + msecs_to_jiffies(TIMEOUT_MS));
+
     return 0;
 }
 
 static void __exit ksysfs_logger_exit(void) {
     pr_info("ksysfs logger exit\n");
+
+    del_timer_sync(&write_msg_timer);
 }
 
 module_init(ksysfs_logger_init);
